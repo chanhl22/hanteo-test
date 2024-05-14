104a2f43c599f864125b257d1ec4a60edd3eea0c
chanhl22 <lch9502@naver.com>
2024-05-14 20:50:30 +0900

    init

---
+++ src/main/java/hanteo/hanteotest/HanteoTestApplication.java
@@ -0,0 +1,13 @@
+package hanteo.hanteotest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanteoTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HanteoTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hanteo/hanteotest/HanteoTestApplicationTests.java
@@ -0,0 +1,13 @@
+package hanteo.hanteotest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanteoTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
