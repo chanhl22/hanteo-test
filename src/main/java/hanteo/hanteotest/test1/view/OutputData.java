610c1ebbaccac116ff9874d92ad70b92e8457eee
chanhl22 <lch9502@naver.com>
2024-05-15 16:01:54 +0900

    fix: 출력 문자열 수정

--- src/main/java/hanteo/hanteotest/test1/view/OutputData.java
+++ src/main/java/hanteo/hanteotest/test1/view/OutputData.java
@@ -4,9 +4,9 @@ public class OutputData {
 
     private static final String INPUT_SUM_MESSAGE = """
             검색할 카테고리 식별자 또는 카테고리명을 입력해주세요.(쉼표(,) 기준으로 구분)
-            예시1 = 성별,남자
-            예시2 = 그룹,엑소
-            예시2 = 게시판,첸""";
+             - 예시1 = 성별,남자
+             - 예시2 = 그룹,엑소
+             - 예시3 = 게시판,첸""";
     private static final String EXECUTION_RESULT_MESSAGE = "실행 결과";
 
     public static void printSearchInputMessage() {
