25a9672ac9bae974a481a0232bd99e33d2403c7e
chanhl22 <lch9502@naver.com>
2024-05-15 11:55:56 +0900

    refactor: 출력 문자열 수정

--- src/main/java/hanteo/hanteotest/test2/view/OutputData.java
+++ src/main/java/hanteo/hanteotest/test2/view/OutputData.java
@@ -1,10 +1,8 @@
 package hanteo.hanteotest.test2.view;
 
-import java.util.List;
-
 public class OutputData {
 
-    private static final String INPUT_SUM_MESSAGE = "정수의 합계를 입력해주세요";
+    private static final String INPUT_SUM_MESSAGE = "정수의 합계를 입력해주세요.";
     private static final String INPUT_COINS_MESSAGE = "각 동전의 유형을 입력하세요.(동전은 쉼표(,) 기준으로 구분, 예시 = 1,2,3)";
     private static final String EXECUTION_RESULT_MESSAGE = "실행 결과";
 
