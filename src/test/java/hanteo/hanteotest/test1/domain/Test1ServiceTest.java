beaa565deabbf12be14bc19d2e2d6bed72d41f3c
chanhl22 <lch9502@naver.com>
2024-05-15 13:52:52 +0900

    test: 검색 테스트

--- src/test/java/hanteo/hanteotest/test1/domain/Test1ServiceTest.java
+++ src/test/java/hanteo/hanteotest/test1/domain/Test1ServiceTest.java
@@ -60,20 +60,54 @@ class Test1ServiceTest {
         assertThat(text).isEqualTo("[{\"id\":2,\"boardName\":\"첸\"}]");
     }
 
-    @DisplayName("올바른 카테고리 식별자가 아닌 경우 메시지를 JSON 텍스트로 조회한다.")
+    @DisplayName("올바른 카테고리 식별자가 아닌 경우 예외가 발생한다.")
     @Test
     void noCategoryIndex() {
         //given
         Test1Service test1Service = new Test1Service(
                 new DataGenerator());
 
-        String categoryIndex = "잘못됨";
+        String categoryIndex = "없음";
         String categoryName = "";
 
         //when //then
         assertThatThrownBy(() -> test1Service.find(categoryIndex, categoryName))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 입력한 검색어를 다시 확인해주세요.");
+    }
+
+    @DisplayName("해당되는 그룹이 없는 경우 null 값을 넣어서 반환한다.")
+    @Test
+    void noGroup() {
+        //given
+        Test1Service test1Service = new Test1Service(
+                new DataGenerator());
+
+        String categoryIndex = "그룹";
+        String categoryName = "없음";
+
+        //when
+        String text = test1Service.find(categoryIndex, categoryName);
+
+        //then
+        assertThat(text).isEqualTo("{\"id\":0,\"groupName\":null,\"boards\":null}");
+    }
+
+    @DisplayName("해당되는 게시판이 없는 경우 빈 리스트를 반환한다.")
+    @Test
+    void noBoard() {
+        //given
+        Test1Service test1Service = new Test1Service(
+                new DataGenerator());
+
+        String categoryIndex = "게시판";
+        String categoryName = "없음";
+
+        //when
+        String text = test1Service.find(categoryIndex, categoryName);
+
+        //then
+        assertThat(text).isEqualTo("[]");
     }
 
 }
